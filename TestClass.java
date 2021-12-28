39ee3bc19ef5372fbd130302a01027603d2ed24d
emir-aydinn <81522910+emir-aydinn@users.noreply.github.com>
2021-12-28 23:26:59 +0300

    Creating TestClass and initializing it

---
+++ TestClass.java
@@ -0,0 +1,13 @@
+package MineSweepR;
+
+import javax.swing.JFrame;
+
+
+public class TestClass {
+    private static final int NUMBER_OF_MINES = 15;
+    public static final int SIZE = 20;
+    public static void main(String[] args) {
+      
+    }
+  
+}
