07236a9d1efcd2eb0046eb8da769da5b7da06184
volkancebi <77071249+volkancebi@users.noreply.github.com>
2021-12-29 00:05:35 +0300

    gui, username and timer implemented

--- MineSweeperGUI.java
+++ MineSweeperGUI.java
@@ -1,5 +1,3 @@
-package MineSweepR;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,12 +8,24 @@ public class MineSweeperGUI extends JPanel {
   public MineGrid grid;
   public int col, row;
   public JButton[][] myButton;
+  public static Timer timer;
+  private int seconds = 0;
+  private int minutes = 0;
+  final private int delay = 1000;
+  public static String username;
+  JTextField counter = new JTextField();
+  JTextField user = new JTextField();
   
   public MineSweeperGUI(int numRows,int numCols,int numMines){
     setSize(1200,850);
     JPanel panel = new JPanel();
     JPanel panel1 = new JPanel();
     JPanel panel2 = new JPanel();
+    username =JOptionPane.showInputDialog(null, "Enter your username");
+        while (username.equals("")){
+            JOptionPane.showMessageDialog(null, "You must enter a username");
+            username =JOptionPane.showInputDialog(null, "Enter your username");
+        }
     
     grid=new MineGrid(numRows,numCols,numMines);
         myButton = new JButton[numRows][numCols];
@@ -28,9 +38,48 @@ public class MineSweeperGUI extends JPanel {
                 panel1.add(myButton[i][j]);
             }
         }
-        grid.shareButtons(myButton);
+     grid.shareButtons(myButton);
+     panel.setLayout(new java.awt.FlowLayout());
+     panel.setSize(950, 750);
+     panel1.setLayout(new java.awt.GridLayout(20, 20));
+     panel1.setPreferredSize(new Dimension(1000, 700));
+     panel2.setLayout(new java.awt.GridLayout(2,1, 100, 100));
+     panel2.setSize(200,400);
+     counter.setHorizontalAlignment(JTextField.CENTER);
+     counter.setEditable(false);
+     counter.setText("Counter");
+     Font font1 = new Font("SansSerif", Font.BOLD, 30);
+     counter.setFont(font1);
+
+     user.setHorizontalAlignment(JTextField.CENTER);
+     user.setEditable(false);
+     user.setText(username);
+     user.setFont(font1);
+
+     panel2.add(counter);
+     panel2.add(user);
+
+     panel.add(panel1, FlowLayout.LEFT);
+     panel.add(panel2);
+     timer = new Timer(delay, taskPerformer);
+     timer.start();
+     panel.add(panel1);
+     add(panel, FlowLayout.LEFT);
     
   }
+  
+    final private ActionListener taskPerformer = new ActionListener() {
+        public void actionPerformed(ActionEvent evt) {
+            if(seconds < 59) {
+                seconds++;
+            }
+            else {
+                minutes++;
+                seconds = 0;
+            }
+            counter.setText(minutes + " : " + seconds);
+        }
+    };
   
   
 }
