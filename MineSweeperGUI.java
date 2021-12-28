9ba9d2b2dc8087e7c05792b2ea8e7a53adfe1a25
emir-aydinn <81522910+emir-aydinn@users.noreply.github.com>
2021-12-28 23:34:23 +0300

    Creating MineSweeperGUI class

---
+++ MineSweeperGUI.java
@@ -0,0 +1,23 @@
+package MineSweepR;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class MineSweeperGUI extends JPanel {
+  
+  public MineGrid grid;
+  public int col, row;
+  public JButton[][] myButton;
+  
+  public MineSweeperGUI(int numRows,int numCols,int numMines){
+    setSize(1200,850);
+    JPanel panel = new JPanel();
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+    
+  }
+  
+  
+}
