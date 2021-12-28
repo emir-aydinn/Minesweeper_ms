06994684d758b8773f808b5677905f698a5dfa57
emir-aydinn <81522910+emir-aydinn@users.noreply.github.com>
2021-12-28 23:38:48 +0300

    Setting MinesweeperGui as visible

--- TestClass.java
+++ TestClass.java
@@ -6,8 +6,15 @@ import javax.swing.JFrame;
 public class TestClass {
     private static final int NUMBER_OF_MINES = 15;
     public static final int SIZE = 20;
+    
     public static void main(String[] args) {
-      
+        
+      JFrame frame = new JFrame("Mine sweeper " + NUMBER_OF_MINES);
+        frame.add(new MineSweeperGUI(SIZE,SIZE,NUMBER_OF_MINES));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000,600);
+        frame.setVisible(true);
+        
     }
   
 }
