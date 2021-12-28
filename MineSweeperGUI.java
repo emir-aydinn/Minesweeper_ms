1c81e2c174dd4f9be6748ee1048c9e9c40c40297
emir-aydinn <81522910+emir-aydinn@users.noreply.github.com>
2021-12-28 23:36:28 +0300

    Adding buttons on Minesweeper

--- MineSweeperGUI.java
+++ MineSweeperGUI.java
@@ -17,6 +17,19 @@ public class MineSweeperGUI extends JPanel {
     JPanel panel1 = new JPanel();
     JPanel panel2 = new JPanel();
     
+    grid=new MineGrid(numRows,numCols,numMines);
+        myButton = new JButton[numRows][numCols];
+        for(int i=0; i<numRows; i++){
+            for(int j=0; j<numCols; j++){
+                myButton[i][j] =new JButton();
+                ButtonHandler bHandler = new ButtonHandler(i,j,grid, myButton);
+                myButton[i][j].addActionListener(bHandler);
+                myButton[i][j].addMouseListener(bHandler);
+                panel1.add(myButton[i][j]);
+            }
+        }
+        grid.shareButtons(myButton);
+    
   }
   
   
